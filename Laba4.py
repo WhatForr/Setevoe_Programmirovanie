7a49bd46b4f42670b8da18e9cdb824367ea1b65a
WhatForr <132435262+WhatForr@users.noreply.github.com>
2023-05-03 15:56:33 +0800

    Update Laba4.py

--- Laba4.py
+++ Laba4.py
@@ -1,27 +1,27 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # библиотека Python для извлечения данных из файлов HTML и XML
 import requests
 
-URL = "http://biik.ru/rasp/cg109.htm"
-a = requests.get(URL)
-a.encoding="windows-1251"
-#print(a.text)
-if a == 404:
+Template = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
+a = requests.get(Template) # получение запроса
+a.encoding="windows-1251" # кодировка
+print(a.text) # вывод на экран
+if a == 404: # если ошибка
 	print ('Not Found')
 else: 
-	all_web_code = a.text
-	bs =BeautifulSoup(all_web_code,"html.parser")
-	Lesson = bs.findAll('td')
+	html_code = a.text
+	bs =BeautifulSoup(html_code,"html.parser") #парсинг html документа
+	Lesson = bs.findAll('td') # td - столбец
 	
-	file = open ("Raspisanie.txt", "w")
+	file = open ("Timetable.txt", "w") # запись в файл
 	for item in Lesson:
 		file.write(item.text +"\n")
 	file.close()
 
-	line = open ("Raspisanie.txt").readlines()
+	line = open ("Timetable.txt").readlines()
 	for i in range(104):
 		line.pop(0)
 	
-	f = open ("Raspisanie.txt", "w")
+	f = open ("Timetable.txt", "w")
 	for i in line:
 		f.write(i)
 	f.close()
