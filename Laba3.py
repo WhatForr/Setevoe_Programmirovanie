9f6b406c759c6ba4af2af8549246d3c6aa146ac5
WhatForr <132435262+WhatForr@users.noreply.github.com>
2023-05-03 15:55:45 +0800

    Update Laba3.py

--- Laba3.py
+++ Laba3.py
@@ -1,6 +1,6 @@
 import requests
 
-URL = "http://biik.ru/rasp/cg109.htm"
-a = requests.get(URL)
-a.encoding="windows-1251"
-print(a.text)
+Template = "http://biik.ru/rasp/cg109.htm" # Запрос на сайт Биик Сибгути
+a = requests.get(Template) # получение запроса
+a.encoding="windows-1251" # кодировка
+print(a.text) # вывод на экран
