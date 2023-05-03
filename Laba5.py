a5e7ad772cd2a9e1640309f43fb3f9f2e977378f
WhatForr <132435262+WhatForr@users.noreply.github.com>
2023-05-03 16:09:53 +0800

    Update Laba5.py

--- Laba5.py
+++ Laba5.py
@@ -1,12 +1,22 @@
+# Импортируем модуль ftplib для работы с FTP-сервером
+
 from ftplib import FTP
- 
+
+# Устанавливаем соединение с FTP-сервером ftp.gnu.org
+
 ftp = FTP('ftp.gnu.org')
 
-ftp.login() 
+# Авторизуемся на FTP-сервере
 
- 
-# Путь на нашем компьютере где сохранить файл.
+ftp.login()
+
+# Задаем путь и имя файла, который мы будем получать с FTP-сервера
+
 out = '/root/recieve/tree.json.gz'
- 
+
+# Открываем файл на запись в бинарном режиме
+# Выполняем получение файла с FTP-сервера с помощью метода retrbinary
+# Если файл успешно получен, то записываем его содержимое в открытый файл
+
 with open(out, 'wb') as f:
-    ftp.retrbinary('RETR ' + 'tree.json.gz', f.write)
+    ftp.retrbinary('RETR ' + 'tree.json.gz', f.write).
